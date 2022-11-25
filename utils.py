ebc125a35be6dfb21b8016463d76cfab4acba0fd
Nico-Florio <nflorio@fi.uba.ar>
2022-11-24 21:11:24 -0300

    'se crea la funcion obtener_patente()'

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
-API_TOKEN = 'Token 8e0fee00a18a82f4e672f4f1239252435727dbfe'
 import requests
+API_TOKEN = 'Token 8e0fee00a18a82f4e672f4f1239252435727dbfe'
 
-regions: list = ['ar', 'us-ca'] 
-with open(r'imgs\023.png', 'rb') as fp:
-    response = requests.post(
-        'https://api.platerecognizer.com/v1/plate-reader/',
-        data=dict(regions=regions),  
-        files=dict(upload=fp),
-        headers={'Authorization': API_TOKEN})
-    
-plate_number : str = response.json()['results'][0]['plate']
-print(plate_number)
+
+def obtener_patente(ruta_foto: str):        
+        regions: list = ['ar', 'us-ca'] 
+        with open(ruta_foto) as fp:
+            response = requests.post(
+                'https://api.platerecognizer.com/v1/plate-reader/',
+                data=dict(regions=regions),  
+                files=dict(upload=fp),
+                headers={'Authorization': API_TOKEN})
+        patente : str = response.json()['results'][0]['plate']
+        return patente
